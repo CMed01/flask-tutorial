1f3a9db08db91e791ba0adac4c32ce3e4973b318
Christopher Meddings <c.m.meddings@gmail.com>
2023-09-15 09:31:02 +0000

    Add URL links to HTML and python

--- run.py
+++ run.py
@@ -4,11 +4,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+"""
+Code for routing websites to each other
+"""
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     """
